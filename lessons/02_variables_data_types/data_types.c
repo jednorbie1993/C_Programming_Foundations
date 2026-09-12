8bef779d915e58478ffc12a1e45733409f48603e
jednorbie1993 <jedjarin@gmail.com>
2026-09-12 11:37:55 +0900

    data types

---
+++ lessons/02_variables_data_types/data_types.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int age = 33;
+    char initial = 'J';
+    float height = 176.5;
+
+    printf("My initial is %c\n", initial);
+    printf("My age is %d\n", age);
+    printf("My height is %.1f\n", height);
+
+    return 0;
+}
